b710d82e297e79f64653576e8948cd909ee1b2ae
June <khazoda@gmail.com>
2023-03-03 02:52:04 +0000

    details, refactoring

--- src/main/java/net/seacroak/beefandblade/mixin/ExampleMixin.java
+++ src/main/java/net/seacroak/beefandblade/mixin/ExampleMixin.java
@@ -1,7 +1,8 @@
-package net.fabricmc.example.mixin;
+package net.seacroak.beefandblade.mixin;
 
-import net.fabricmc.example.BeefAndBlade;
 import net.minecraft.client.gui.screen.TitleScreen;
+import net.seacroak.beefandblade.BeefAndBlade;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,6 +12,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		BeefAndBlade.LOGGER.info("This line is printed by an example mod mixin!");
+		BeefAndBlade.LOGGER.info("This line is printed by Beef and Blade's mixin");
 	}
 }
